f8c318ded0ea4777bf9e04246a7fc71629aaa1be
김대휘 <kimdaewhi@naver.com>
2023-10-19 12:53:29 +0900

    Emotion 실습2

--- src/components/Emotion/EmotionExam2.jsx
+++ src/components/Emotion/EmotionExam2.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 /** @jsxImportSource @emotion/react */
+import styled from '@emotion/styled';
 
+// =========================== Example 1 =========================== //
 const P = props => (
     <p
         css={{
@@ -37,6 +39,35 @@ const SmallArticleText = props => {
     );
 }
 
+// =========================== Example 2 =========================== //
+
+const Button = styled.button`
+    color: turquoise;
+    margin: 5px;
+`;
+
+// 기본 butotn을 상속받아 사용하는 법
+// const Button2 = styled.Button`
+//     color: ${props => (props.primary ? 'hotpink' : 'turquoise')};
+// `;
+
+// 내가 만든 커스텀 컴포넌트를 상속받아 사용하는법
+const Button2 = styled(Button)`
+    color: ${props => (props.primary ? 'hotpink' : 'turquoise')};
+`;
+
+const Container = styled.div(props => ({
+    display: 'flex',
+    flexDirection: props.column && 'column'
+}));
+
+// =========================== Example 3 =========================== //
+
+const Basic = ({ className }) => <div className={className}>Some text</div>
+
+const Fancy = styled(Basic)`
+    color: hotpink;
+`;
 export default function EmotionExam2() {
     return (
         <>
@@ -55,6 +86,19 @@ export default function EmotionExam2() {
             <P>P Style Tag</P>
             <ArticleText>ArticleText</ArticleText>
             <SmallArticleText>Small Article Text</SmallArticleText>
+
+            <br/>
+
+            <Button>This my button button.</Button>
+            <br/>
+            <Container column>
+                <Button2>This is a regular button.</Button2>
+                <Button2 primary>This is a primary button.</Button2>
+            </Container>
+
+            <br/>
+
+            <Fancy/>
         </>
     )
 }
